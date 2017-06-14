6db8751cff7fb2a8f5c6cec1e889d078a656d3a4
Denis Volok <denis.omen.x@gmail.com>
2017-06-14 19:14:23 +0300

    added Planix

---
+++ websites/Pragmatica/Planix/js/common.js
@@ -0,0 +1,154 @@
+'use strict';
+
+// ========>> DEFAULT VARIABLES <<========
+
+// Define touch devices
+var isTouch = 'ontouchstart' in window || navigator.msMaxTouchPoints > 0;
+
+// +++++++++++++++ FUNCTIONS +++++++++++++++
+
+
+// ========>> WELCOME SLIDER <<========
+
+
+function welcomeSlider() {
+	if ($('.welcome-slider').length) {
+		$('.welcome-slider').slick({
+			infiniti: true,
+			slidesToShow: 1,
+			slidesToScroll: 1,
+			dots: true,
+			arrows: false,
+			// autoplay: true,
+			autoplaySpeed: 5000,
+			speed: 1100,
+			cssEase: 'cubic-bezier(.38,.2,.25,.97)'
+		});
+	}
+}
+
+// ========>> SCROLL TO TOP <<========
+
+function scrollToTop() {
+	var toTop = $('#to-top');
+
+	toTop.on('click', function () {
+		$('html, body').animate({ scrollTop: 0 }, 550);
+	});
+
+	window.addEventListener('scroll', function (e) {
+		showScroll();
+	});
+
+	function showScroll() {
+		if (window.pageYOffset > document.documentElement.clientHeight) {
+			toTop.addClass('to-top_visible');
+		} else {
+			toTop.removeClass('to-top_visible');
+		}
+	}
+
+	showScroll();
+}
+
+// ========>> MAIN NAV <<========
+
+var navList = $('nav .nav__list-wrap');
+var navButton = $('nav .nav__mob-button, .nav-mob-button');
+var navShade = $('nav .nav__shade');
+
+function mainNavMenu() {
+
+	navList.addClass('animate');
+	setTimeout(function () {
+		navList.addClass('trs');
+	}, 0);
+
+	function navToggle() {
+
+		if (navList.hasClass('animate')) {
+			$(navShade).on('click', navToggle);
+			$(navList).toggleClass('animate');
+
+			$(navShade).css('display', 'block');
+			$(navShade).stop().animate({ opacity: '.7' }, 300);
+		} else {
+			$(navList).toggleClass('animate');
+			$(navShade).stop().animate({ opacity: '0' }, 300);
+
+			$(navShade).off('click', navToggle); // fix fast double-click
+
+			setTimeout(function () {
+				$(navShade).css('display', 'none');
+			}, 300);
+		}
+	}
+
+	$(navButton).on('click', navToggle);
+}
+
+// ========>> EQUAL HEIGHT <<========
+
+function servicesEqualHeight() {
+	if ($('.service').length) {
+		$(function () {
+			$('.service__item-img').matchHeight({
+				byRow: true,
+				property: 'height',
+				target: null,
+				remove: false
+			});
+		});
+	}
+}
+
+// ========>> DOCUMENT READY <<========
+
+$(function () {
+
+	// Preloader
+
+	$('#loader').fadeOut(200);
+	setTimeout(function () {
+		$('#loader').remove();
+	}, 210);
+
+	// ========>> FUNCTION'S CALL <<========
+
+	welcomeSlider();
+	scrollToTop();
+	mainNavMenu();
+	servicesEqualHeight();
+
+	// ========>> RESIZE <<========
+
+
+	$(window).on('resize', function () {
+
+		if ($(window).width() >= 768) {
+			$(navList).addClass('animate');
+			$(navShade).css('display', '');
+		}
+	}).resize(); // end resize
+}); // end document ready
+
+
+// ========>> DETECT CLIENT CONFIG <<========
+
+// Detect touch devices
+
+if (isTouch) {
+	$(document.body).addClass('touch');
+} else {
+	$(document.body).addClass('no-touch');
+}
+
+// Detect disabled JS
+
+$(document.body).removeClass('no-js');
+
+// Prevent dragging img, links
+
+$("img, a").on("dragstart", function (event) {
+	event.preventDefault();
+});
