4281988e78541f53a38a5d2a7dab7ab9ab8a324e
Denis Volok <denis.omen.x@gmail.com>
2017-06-15 23:11:48 +0300

    update Rem

--- websites/Rem/js/common.js
+++ websites/Rem/js/common.js
@@ -1,7 +1,93 @@
 'use strict';
 
+function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
+
 // ========>> DEFAULT VARIABLES <<========
-// Detect disabled JS
+
+// ========>> Array.from POLYFILL <<========
+/*eslint-disable*/
+if (!Array.from) {
+  Array.from = function () {
+    var toStr = Object.prototype.toString;
+    var isCallable = function isCallable(fn) {
+      return typeof fn === 'function' || toStr.call(fn) === '[object Function]';
+    };
+    var toInteger = function toInteger(value) {
+      var number = Number(value);
+      if (isNaN(number)) {
+        return 0;
+      }
+      if (number === 0 || !isFinite(number)) {
+        return number;
+      }
+      return (number > 0 ? 1 : -1) * Math.floor(Math.abs(number));
+    };
+    var maxSafeInteger = Math.pow(2, 53) - 1;
+    var toLength = function toLength(value) {
+      var len = toInteger(value);
+      return Math.min(Math.max(len, 0), maxSafeInteger);
+    };
+
+    // Свойство length метода from равно 1.
+    return function from(arrayLike /*, mapFn, thisArg */) {
+      // 1. Положим C равным значению this.
+      var C = this;
+
+      // 2. Положим items равным ToObject(arrayLike).
+      var items = Object(arrayLike);
+
+      // 3. ReturnIfAbrupt(items).
+      if (arrayLike == null) {
+        throw new TypeError('Array.from requires an array-like object - not null or undefined');
+      }
+
+      // 4. Если mapfn равен undefined, положим mapping равным false.
+      var mapFn = arguments[1];
+      if (typeof mapFn !== 'undefined') {
+        mapFn = arguments.length > 1 ? arguments[1] : void undefined;
+        // 5. иначе
+        // 5. a. Если вызов IsCallable(mapfn) равен false, выкидываем исключение TypeError.
+        if (!isCallable(mapFn)) {
+          throw new TypeError('Array.from: when provided, the second argument must be a function');
+        }
+
+        // 5. b. Если thisArg присутствует, положим T равным thisArg; иначе положим T равным undefined.
+        if (arguments.length > 2) {
+          T = arguments[2];
+        }
+      }
+
+      // 10. Положим lenValue равным Get(items, "length").
+      // 11. Положим len равным ToLength(lenValue).
+      var len = toLength(items.length);
+
+      // 13. Если IsConstructor(C) равен true, то
+      // 13. a. Положим A равным результату вызова внутреннего метода [[Construct]]
+      //     объекта C со списком аргументов, содержащим единственный элемент len.
+      // 14. a. Иначе, положим A равным ArrayCreate(len).
+      var A = isCallable(C) ? Object(new C(len)) : new Array(len);
+
+      // 16. Положим k равным 0.
+      var k = 0;
+      // 17. Пока k < len, будем повторять... (шаги с a по h)
+      var kValue;
+      while (k < len) {
+        kValue = items[k];
+        if (mapFn) {
+          A[k] = typeof T === 'undefined' ? mapFn(kValue, k) : mapFn.call(T, kValue, k);
+        } else {
+          A[k] = kValue;
+        }
+        k += 1;
+      }
+      // 18. Положим putStatus равным Put(A, "length", len, true).
+      A.length = len;
+      // 20. Вернём A.
+      return A;
+    };
+  }();
+}
+/*eslint-enable*/
 
 // Define touch devices
 
@@ -51,27 +137,6 @@ var checkLoad = function checkLoad() {
 checkLoad();
 
 function documentReady() {
-  // ========>> ANIMATION <<========
-
-  // // Adding animation classes
-
-  // const animClasses = [
-  // '.select',
-  // ];
-
-  // animClasses.forEach((select) => {
-  //   $(select).addClass('animate');
-  // });
-
-  // // fix transition effect after page reloading,
-  // // transition adds after animation class,
-  // // all transitions must be listed in this selector
-
-  // setTimeout(() => {
-  //   $('selector')
-  //   .addClass('trs');
-  // }, 0);
-
 
   // COPYRIGHT
 
@@ -117,6 +182,61 @@ function documentReady() {
   showToTopBtn();
 
   // end to-top
+
+
+  // ========>> FORM <<========
+
+  var feedback = document.querySelector('.form-feedback');
+  var feedbackSuccesMsg = document.querySelector('.feedback__succes');
+
+  feedback.addEventListener('submit', function (e) {
+    // const formThank = $('.form__thank', e.target);
+    e.preventDefault();
+
+    // feedbackSuccesMsg.classList.add('active');
+    // feedbackSuccesMsg.classList.remove('hidden');
+    // setTimeout(() => {
+    //   feedbackSuccesMsg.classList.remove('active');
+    //   setTimeout(() => {
+    //     feedbackSuccesMsg.classList.add('hidden');
+    //   }, 300);
+    // }, 1400);
+
+    var inputs = [];
+    [].concat(_toConsumableArray(feedback.elements)).map(function (elem) {
+      if (elem.type !== 'submit') inputs.push(elem);
+    });
+
+    var body = inputs.reduce(function (acc, elem) {
+      if (typeof acc !== 'string') return acc.name + '=' + acc.value + '&' + elem.name + '=' + elem.value;
+      return acc + '&' + elem.name + '=' + elem.value;
+    });
+
+    var xhr = new XMLHttpRequest();
+
+    xhr.open('POST', 'mail.php', true);
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.send(body);
+
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState === 4) {
+        if (xhr.status === 200) {
+          console.log('SUCCES Request');
+          feedback.reset();
+          feedbackSuccesMsg.classList.remove('hidden');
+          feedbackSuccesMsg.classList.add('active');
+          setTimeout(function () {
+            feedbackSuccesMsg.classList.remove('active');
+            setTimeout(function () {
+              feedbackSuccesMsg.classList.add('hidden');
+            }, 300);
+          }, 1400);
+        } else console.log('FAILED Request');
+      }
+    };
+  });
+
+  // end form
 
 
   // ========>> GLOBAL EVENTS <<========
