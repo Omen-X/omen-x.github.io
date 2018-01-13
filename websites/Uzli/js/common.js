9741aa625d5f776e7d84aee3588eb9bb7a6fc9ab
Denis Volok <denis.omen.x@gmail.com>
2018-01-14 00:09:58 +0300

    f

--- websites/Uzli/js/common.js
+++ websites/Uzli/js/common.js
@@ -3,10 +3,119 @@
 // ========>> FUNCTIONS <<========
 
 function welcomeCarousel() {
-  if ($('.welcome-carousel').length) {
+  if ($('.welcome-carousel').length && window.matchMedia('(min-width: 900px)').matches) {
     $('.welcome-carousel').lightSlider({
       item: 1,
-      slideMargin: 0
+      slideMargin: 0,
+      loop: true
+    });
+  }
+}
+
+function catalogProductsCarousel() {
+  if ($('.catalog-products').length && window.matchMedia('(max-width: 899px)').matches) {
+    $('.catalog-products').lightSlider({
+      item: 1,
+      loop: true,
+      pager: false
+    });
+  }
+}
+
+function clientsCarousel() {
+  if ($('.clients__items').length && window.matchMedia('(max-width: 899px)').matches) {
+    $('.clients__items').lightSlider({
+      item: 1,
+      loop: true,
+      pager: false
+    });
+  }
+}
+
+function myCounter() {
+  if ($('.my-counter').length) {
+    var $btnUp = $('.my-counter__btn-up');
+    var $btnDown = $('.my-counter__btn-down');
+    var disabledClass = 'my-counter__btn_disabled';
+
+    $btnUp.click(function (event) {
+      var $btn = $(event.target);
+      var $field = $btn.siblings('.my-counter__value');
+      var value = +$field.text();
+
+      $field.text(value + 1);
+      if (value > 1) $btn.siblings('.my-counter__btn-down').removeClass(disabledClass);
+    });
+
+    $btnDown.click(function (event) {
+      var $btn = $(event.target);
+      var $field = $btn.siblings('.my-counter__value');
+      var value = +$field.text();
+
+      if (value <= 1) {
+        $btn.addClass(disabledClass);
+        return;
+      }
+
+      $field.text(value - 1);
+    });
+  }
+}
+
+function mySelect() {
+  if ($('.my-select').length) {
+    var $values = $('.my-select__values span');
+
+    $values.click(function () {
+      var $target = $(this);
+      var $field = $target.parent().siblings('.my-select__value');
+
+      $field.text($target.text());
+    });
+  }
+}
+
+function mySearch() {
+  if ($('.my-search').length) {
+    var form = $('.my-search');
+    var input = $('.my-search__field');
+
+    if (window.matchMedia('(min-width: 900px)').matches) {
+      form.hover(function () {
+        form.addClass('my-search_hover');
+        input.focus();
+      }, function () {
+        if (input[0].value.length > 0) return;
+        form.removeClass('my-search_hover');
+      });
+    }
+    if (window.matchMedia('(max-width: 899px)').matches) {
+      input[0].placeholder = 'Поиск';
+    }
+  }
+}
+
+function advantages() {
+  if ($('.advantages').length) {
+    if (window.matchMedia('(max-width: 899px)').matches) {
+      var imgs = $('.advantages__item-img img');
+
+      $.each(imgs, function (i, img) {
+        var src = $(img).data('mob-img');
+        $(img).attr('src', src);
+      });
+    }
+  }
+}
+
+function basketPage() {
+  if ($('.basket-wrap').length) {
+    var $btnRemove = $('.product-table__btn-remove');
+
+    $btnRemove.click(function () {
+      var $target = $(this);
+
+      $target.closest('.product-table__row').slideUp(320);
     });
   }
 }
@@ -77,6 +186,13 @@ function documentReady() {
 // ========>> FUNCTIONS CALL <<========
 
 welcomeCarousel();
+catalogProductsCarousel();
+clientsCarousel();
+mySearch();
+advantages();
+myCounter();
+mySelect();
+basketPage();
 
 // ========>> UTILS <<========
 
