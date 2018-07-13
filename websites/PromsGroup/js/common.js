2f125f94cdba0caf419c16b04bab4d0b668f5898
Denis Volok <denis.omen.x@gmail.com>
2018-07-13 10:36:37 +0300

    F

--- websites/PromsGroup/js/common.js
+++ websites/PromsGroup/js/common.js
@@ -6,6 +6,41 @@ var basketPopover = function basketPopover() {
   if ($(".basket-block__popover").length) {}
 };
 
+var welcomeCarousel = function welcomeCarousel() {
+  if ($(".welcome-carousel").length) {
+    var activeClass = "welcome__nav-item_active";
+
+    var slider = $(".welcome-carousel").lightSlider({
+      item: 1,
+      controls: false,
+      loop: true,
+      speed: 1000,
+      mode: 'fade',
+      auto: true,
+      pauseOnHover: true,
+      pause: 4000,
+      pager: false,
+      onBeforeNextSlide: function onBeforeNextSlide() {
+        var $nextItem = $("." + activeClass + " + .welcome__nav-item");
+
+        if ($nextItem.length === 0) $nextItem = $('.welcome__nav-item:first-child');
+
+        $('.welcome__nav-item').removeClass(activeClass);
+        $nextItem.addClass(activeClass);
+      }
+    });
+
+    $(".welcome__nav-item").each(function (i, e) {
+      $(e).click(function () {
+        $(".welcome__nav-item").removeClass(activeClass);
+
+        slider.goToSlide(i + 1);
+        $(e).addClass(activeClass);
+      });
+    });
+  }
+};
+
 // ========>> DOCUMENT READY <<========
 
 function documentReady() {
@@ -13,6 +48,7 @@ function documentReady() {
   // ========>> FUNCTIONS CALL <<========
 
   basketPopover();
+  welcomeCarousel();
 } // end document ready
 
 // ========>> UTILS <<========
